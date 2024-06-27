c022e2d24cc7b5289ed4360512671874bf021002
Андрей Никитин <146591318+AndrewNikkk@users.noreply.github.com>
2024-06-27 22:12:58 +0300

    больше данных, перед переделкой

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import fake_useragent
+import time
+import json
+
+
+def get_links(text):
+    pass
+
+def get_vacancy(link):
+    pass
+
 
 url = "https://hh.ru/search/vacancy?page=0&disableBrowserCache=true&hhtmFrom=vacancy_search_list"
 headers = {
@@ -7,17 +18,25 @@ headers = {
 }
 
 response = requests.get(url, headers=headers)
-
 soup = BeautifulSoup(response.text, "lxml")
 
-data = soup.find("div", class_="vacancy-card--z_UXteNo7bRGzxWVcL7y font-inter")
-compensation_labels_data = data.find("div", class_="compensation-labels--uUto71l5gcnhU2I8TZmz")
+data = soup.find_all("div", class_="vacancy-card--z_UXteNo7bRGzxWVcL7y font-inter")
 
-name = data.find("span", class_="vacancy-name--c1Lay3KouCl7XasYakLk serp-item__title-link").text
-salary = data.find("span", class_="fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni compensation-text--kTJ0_rp54B2vNeZ3CTt2 separate-line-on-xs--mtby5gO4J0ixtqzW38wh").text
-experience = compensation_labels_data.find("span", class_="label--rWRLMsbliNlu_OMkM_D3 label_light-gray--naceJW1Byb6XTGCkZtUM").text
+for i in data:
+    mini_info = i.find("div", class_="info-section--N695JG77kqwzxWAnSePt")
+
+    name = i.find("span", class_="vacancy-name--c1Lay3KouCl7XasYakLk serp-item__title-link").text
+    salary = i.find("span", class_="fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni compensation-text--kTJ0_rp54B2vNeZ3CTt2 separate-line-on-xs--mtby5gO4J0ixtqzW38wh").text
+    link = i.find("a", class_="bloko-link").get("href")
+    employer = mini_info.find("span", class_="separate-line-on-xs--mtby5gO4J0ixtqzW38wh").text
+    address = mini_info.find("span", class_="fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni").text
+
+    print(name)
+    print(salary)
+    print(link)
+    print(employer)
+    print(address)
 
 
-print(name)
-print(salary)
-print(experience)
+
+
